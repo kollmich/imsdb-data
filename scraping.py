edb08eb7f84ad561cc1d834daf6a14562b3d08fe
Michal Kollar <koll44@gmail.com>
2019-11-18 22:07:31 +0000

    scrape

--- scraping.py
+++ scraping.py
@@ -3,22 +3,35 @@ import html5lib
 from bs4 import BeautifulSoup 
 import re
 
-URL = "https://www.imsdb.com/Movie%20Scripts/Jackie%20Brown%20Script.html"
+URL = "https://www.imsdb.com/Movie%20Scripts/Collateral%20Script.html"
 r = requests.get(URL) 
 
 soup = BeautifulSoup(r.content, 'html5lib') 
 
 script_details = soup.find('table', attrs = {'class':'script-details'})
 
-
-
 title = script_details.h1.text.replace(' Script', '')
 rating = script_details.tbody.contents[2].find(string="Average user rating").next_element.next_element.next_element.next_element.replace(' (','')[0:3]
-author = script_details.tbody.contents[2].find(string="Writers").next_element.next_element.next_element.text
+author_start = script_details.tbody.contents[2].find(string="Writers")
+authors = []
+next = author_start.next_element
+while next.name != 'b':
+    if next.name == 'a':
+        authors.append(next.text)
+    next = next.next_element
+
+genres_start = script_details.tbody.contents[2].find(string="Genres")
+genres = []
+next = genres_start.next_element
+while next.name != 'b':
+    if next.name == 'a':
+        genres.append(next.text)
+    next = next.next_element
+
 date = script_details.tbody.contents[2].find(string="Script Date").next_element.replace(' : ','')
-genres = script_details.tbody.contents[2].find(string="Genres")
 
 print(title)
 print(rating)
-print(author)
+print(authors)
+print(genres)
 print(date)
