dd50afee204b082196d599a3460d012c2fcc3a86
Michal Kollar <koll44@gmail.com>
2019-11-19 21:44:54 +0000

    more scraping

--- scraping.py
+++ scraping.py
@@ -3,35 +3,66 @@ import html5lib
 from bs4 import BeautifulSoup 
 import re
 
-URL = "https://www.imsdb.com/Movie%20Scripts/Collateral%20Script.html"
-r = requests.get(URL) 
+movies_list = ['Collateral','Alien']
 
-soup = BeautifulSoup(r.content, 'html5lib') 
+movies = []
 
-script_details = soup.find('table', attrs = {'class':'script-details'})
+class movie():
+    def __init__(self):
+        self.title = ""
+        self.rating = ""
+        self.authors = ""
+        self.genres = ""
+        self.date = ""
 
-title = script_details.h1.text.replace(' Script', '')
-rating = script_details.tbody.contents[2].find(string="Average user rating").next_element.next_element.next_element.next_element.replace(' (','')[0:3]
-author_start = script_details.tbody.contents[2].find(string="Writers")
-authors = []
-next = author_start.next_element
-while next.name != 'b':
-    if next.name == 'a':
-        authors.append(next.text)
-    next = next.next_element
+    def __repr__(self):
+        return str(self)
 
-genres_start = script_details.tbody.contents[2].find(string="Genres")
-genres = []
-next = genres_start.next_element
-while next.name != 'b':
-    if next.name == 'a':
-        genres.append(next.text)
-    next = next.next_element
+for i in movies_list:
 
-date = script_details.tbody.contents[2].find(string="Script Date").next_element.replace(' : ','')
+    URL_INFO = "https://www.imsdb.com/Movie%20Scripts/" + i + "%20Script.html"
+    URL_SCRIPT = "https://www.imsdb.com/scripts/" + i + ".html"
 
-print(title)
-print(rating)
-print(authors)
-print(genres)
-print(date)
+    r = requests.get(URL_INFO) 
+
+    soup = BeautifulSoup(r.content, 'html5lib') 
+
+    script_details = soup.find('table', attrs = {'class':'script-details'})
+
+    title = script_details.h1.text.replace(' Script', '')
+    rating = script_details.tbody.contents[2].find(string="Average user rating").next_element.next_element.next_element.next_element.replace(' (','')[0:3]
+    author_start = script_details.tbody.contents[2].find(string="Writers")
+    authors = []
+    next = author_start.next_element
+    while next.name != 'b':
+        if next.name == 'a':
+            authors.append(next.text)
+        next = next.next_element
+    separator = ', '
+    authors = separator.join(authors)
+
+    genres_start = script_details.tbody.contents[2].find(string="Genres")
+    genres = []
+    next = genres_start.next_element
+    while next.name != 'b':
+        if next.name == 'a':
+            genres.append(next.text)
+        next = next.next_element
+
+    genres = separator.join(genres)
+
+    date = script_details.tbody.contents[2].find(string="Script Date").next_element.replace(' : ','')
+
+    movie.title = title
+    movie.rating = rating
+    movie.authors = authors
+    movie.genres = genres
+    movie.date = date
+
+    movies.append(movie)
+
+    print(movie.title)
+
+for movie in movies:
+    print(movie.rating)
+
